a4a16b4e227b5781b301a06182456be8ec141289
Thomas Gosselin <1961579@cgmatane.qc.ca>
2021-11-25 09:53:14 -0500

    instauration d'un script (non final) pour pouvoir changer le texte de la balise title, sera utile pour lorsque nous écouterons une musique en particulier

--- script/aquafy.js
+++ script/aquafy.js
@@ -1,1 +1,11 @@
-//Déclarations de variables
+//Déclarations de variable
+
+
+
+/* pour le changement de la balise title */
+let titlePage = document.querySelector("head title");
+
+console.log(titlePage);
+
+titlePage.textContent = "test";
+/*********************************/
