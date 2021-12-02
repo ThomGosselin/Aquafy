c36bb28dff79e48ef6827a7d5f2a258d133771aa
Thomas Gosselin <1961579@cgmatane.qc.ca>
2021-12-02 09:26:30 -0500

    fix du visualizer

--- script/aquafy.js
+++ script/aquafy.js
@@ -18,43 +18,50 @@ const audioElement = document.querySelector("audio");
 const visualizer = document.querySelector(".visualizer");
 
 bouton.addEventListener("click", e =>{
+    if (ctx == undefined){
+        loadVisualizer();
+    }
     audioElement.paused ? audioElement.play() : audioElement.pause();
     bouton.classList.toggle("btn-play");
     bouton.classList.toggle("btn-pause");
 });
+let ctx;
 
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-const ctx = new window.AudioContext();
-const analyser = ctx.createAnalyser();
-const source = ctx.createMediaElementSource(audioElement);
-source.connect(analyser);
-source.connect(ctx.destination);
-analyser.fftSize = 64;
-const bufferLenght = analyser.frequencyBinCount;
-
-let dataArray = new Uint8Array(bufferLenght);
-let elements=[];
-for(let i=0 ; i< bufferLenght; i++){
-    const element = document.createElement('span');
-    element.classList.add('element');
-    elements.push(element);
-    visualizer.appendChild(element);
-}
-
-const clamp = (num, min, max) =>{
-    if(num >= max) return max;
-    if(num <= min) return min;
-    return num;
-}
-
-const update = () =>{
-    requestAnimationFrame(update);
-    analyser.getByteFrequencyData(dataArray);
-    for(let i=0; i< bufferLenght; i++){
-        let item = dataArray[i];
-        item = item > 150 ? item / 1.5 : item * 1.25;
-        elements[i].style.transform = `rotateZ(${i * (30 / bufferLenght)}deg) translate(-50%, ${clamp(item,100, 150)}px)`;
+function loadVisualizer(){
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    ctx = new window.AudioContext();
+    const analyser = ctx.createAnalyser();
+    const source = ctx.createMediaElementSource(audioElement);
+    source.connect(analyser);
+    source.connect(ctx.destination);
+    analyser.fftSize = 64;
+    const bufferLenght = analyser.frequencyBinCount;
+    
+    let dataArray = new Uint8Array(bufferLenght);
+    let elements=[];
+    for(let i=0 ; i< bufferLenght; i++){
+        const element = document.createElement('span');
+        element.classList.add('element');
+        elements.push(element);
+        visualizer.appendChild(element);
     }
-};
-
-update();
+    
+    const clamp = (num, min, max) =>{
+        if(num >= max) return max;
+        if(num <= min) return min;
+        return num;
+    }
+    
+    const update = () =>{
+        requestAnimationFrame(update);
+        analyser.getByteFrequencyData(dataArray);
+        for(let i=0; i< bufferLenght; i++){
+            let item = dataArray[i];
+            item = item > 150 ? item / 1.5 : item * 1.25;
+            elements[i].style.transform = `rotateZ(${i * (30 / bufferLenght)}deg) translate(-50%, ${clamp(item,100, 150)}px)`;
+        }
+    };
+    
+    update();
+    
+}
