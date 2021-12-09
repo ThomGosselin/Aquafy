341d7a097e54ac58b5261635ac7f7fa906b585d2
Thomas Gosselin <1961579@cgmatane.qc.ca>
2021-12-09 09:48:27 -0500

    allsongs array complete

--- script/aquafy.js
+++ script/aquafy.js
@@ -8,61 +8,92 @@ let allMusicArray=[
         nom = "Drown",
         src = "url(../music/DylanMathew/D1.mp3)",
         auteur = "Dylan Matthew",
-        couleurBG = "#fff"
+        couleurBG = "#fff",
+        selector = "dylan1"
     },
     {
         nom = "Don't Forget",
         src = "url(../music/DylanMathew/D2.mp3)",
         auteur = "Dylan Matthew",
-        couleurBG = "#fff"  
+        couleurBG = "#fff",
+        selector = "dylan2"  
     },
     {
         nom = "The Fall",
         src = "url(../music/DylanMathew/D3.mp3)",
         auteur = "Dylan Matthew",
-        couleurBG = "#fff"  
+        couleurBG = "#fff",
+        selector = "dylan3" 
     },
     {
         nom = "Midnight In Tokyo",
         src = "url(../music/DylanMathew/D4.mp3)",
         auteur = "Dylan Matthew",
-        couleurBG = "#fff"  
+        couleurBG = "#fff",
+        selector = "dylan4"  
     },
     {
         nom = "Anxiety",
         src = "url(../music/DylanMathew/anxiety.mp3)",
         auteur = "Dylan Matthew",
-        couleurBG = "#fff"  
+        couleurBG = "#fff",
+        selector = "dylan5"  
     },
     {
         nom = "Saturday Night",
         src = "url(../music/DylanMathew/SaturdayNight.mp3)",
         auteur = "Dylan Matthew",
-        couleurBG = "#fff"  
+        couleurBG = "#fff",
+        selector = "dylan6"  
     },
     {
         nom = "All girls Are the Same",
         src = "url(../music/Juice/allGirlsAreTheSame.mp3)",
         auteur = "Juice Wrld",
-        couleurBG = "#e1e1e1"  
+        couleurBG = "#e1e1e1", 
+        selector = "juice1" 
     },
     {
         nom = "Intro",
         src = "url(../music/Juice/intro.mp3)",
         auteur = "Juice Wrld",
-        couleurBG = "#e1e1e1"  
+        couleurBG = "#e1e1e1",
+        selector = "juice2"  
     },
     {
         nom = "Lean With Me",
         src = "url(../music/Juice/LeanWitMe.mp3)",
         auteur = "Juice Wrld",
-        couleurBG = "#e1e1e1"  
+        couleurBG = "#e1e1e1",
+        selector = "juice3"  
     },
     {
         nom = "Lucid Dreams",
         src = "url(../music/Juice/LucidDreams.mp3)",
         auteur = "Juice Wrld",
-        couleurBG = "#e1e1e1"  
+        couleurBG = "#e1e1e1",
+        selector = "juice4"  
+    },
+    {
+        nom = "Giants",
+        src = "url(../music/Juice/Giants.mp3)",
+        auteur = "No Copyright Songs",
+        couleurBG = "#a1a1a1",
+        selector = "ncs1"  
+    },
+    {
+        nom = "Island",
+        src = "url(../music/Juice/Island.mp3)",
+        auteur = "No Copyright Songs",
+        couleurBG = "#a1a1a1",
+        selector = "ncs2"  
+    },
+    {
+        nom = "Lune",
+        src = "url(../music/Juice/Lune.mp3)",
+        auteur = "No Copyright Songs",
+        couleurBG = "#a1a1a1",
+        selector = "ncs3"  
     },
 
 ];
